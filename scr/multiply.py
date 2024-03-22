37bfa96e7835bf96ce8f187e0dd7e2e9df92fbd2
Florent ATCHEAKOU <atcheakouf@gmail.com>
2024-03-22 15:24:25 +0100

    ecriture de la fontion

---
+++ scr/multiply.py
@@ -0,0 +1,9 @@
+def multiply(a, b):
+    """
+    Multiplies two numbers.
+
+    :param a: First number.
+    :param b: Second number.
+    :return: The product of a and b.
+    """
+    return a * b
