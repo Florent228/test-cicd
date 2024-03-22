09c4df787c3a28f6158d1aaa683b048c36dcd5ac
Florent ATCHEAKOU <atcheakouf@gmail.com>
2024-03-22 17:04:53 +0100

    test positif

--- tests/test_multiply.py
+++ tests/test_multiply.py
@@ -3,7 +3,7 @@ from src.multiply import multiply
 
 class TestMultiply(unittest.TestCase):
     def test_multiply_success(self):
-        self.assertEqual(multiply(2, 3), 7)  # Correction pour réussir
+        self.assertEqual(multiply(2, 3), 6)  # Correction pour réussir
 
 if __name__ == '__main__':
     unittest.main()
